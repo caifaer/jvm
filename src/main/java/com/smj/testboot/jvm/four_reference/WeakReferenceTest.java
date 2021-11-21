33606b6bb3b0362d4251f270a9c508adcdfb71cf
shen'meng'jie <565611652@qq.com>
2021-11-21 18:50:01 +0800

    弱引用

---
+++ src/main/java/com/smj/testboot/jvm/four_reference/WeakReferenceTest.java
@@ -0,0 +1,24 @@
+package com.smj.testboot.jvm.four_reference;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeakReferenceTest {
+    private static final int _4MB = 4 * 1024 * 1024;
+
+    public static void main(String[] args) {
+        //  list --> WeakReference --> byte[]
+        List<WeakReference<byte[]>> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            WeakReference<byte[]> ref = new WeakReference<>(new byte[_4MB]);
+            list.add(ref);
+            for (WeakReference<byte[]> w : list) {
+                System.out.print(w.get()+" ");
+            }
+            System.out.println();
+
+        }
+        System.out.println("循环结束：" + list.size());
+    }
+}
